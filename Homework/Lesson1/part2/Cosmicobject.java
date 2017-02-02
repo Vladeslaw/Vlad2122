e4fcb0086ef2d3d41e5fa1f4b65dd2d24ef244fd
Vladeslaw <vlaulz90@gmail.com>
2017-02-02 19:23:40 +0200

    added full code for CosmicObject class java file

--- Homework/Lesson1/part2/Cosmicobject.java
+++ Homework/Lesson1/part2/Cosmicobject.java
@@ -1,24 +1,107 @@
-public class Cosmicobject{
-	public char id; // full id of the cosmic object 
-	private char elem; // prevailing chemical element of the cosmic object
-	private float diam; // object diameter in kilometers
-	private float sqr; // approximate object square in kilometers
-	private float mas; // approximate object mass in millions of kilograms
-	private float spd; // speed of object in realtion to observer in kilometers per hour
-	public float dis; // distance from observer to object in kilometers
-	public int age; // approximate age of the object in years
-	public int satn; // nuber of satelites orbiting
-	public boolean vis; // can be seen by observer without special equipment
-	
-	public doTrck(); // to do a tracking of the object
-	public doFol(); // to follow an object
-	public doAn(); // to do an anylyze of object
-	public makeProb(); // to take a sample of object
-	private doCol(); // to collect a whole object
-	private doHarv(); // to harvest a material from object
-	private chngTraj(); // to influence an object trajectory
-	private doImp(); //  to initiate an impact onto object
-	public makeStr(); // to place something on object
-	private doFroz(); // to perform a frozing of the object
-	private doHeat(); // to perform a heating of the object
+package cosmicObject;
+
+public class CosmicObject {
+
+	private String cosmicObjectId; 
+	private String cosmicObjectPrevailimgElement;
+	private double cosmicObjectDiameter;
+	private double cosmicObjectSquare;
+	private double cosmicObjectMass;
+	private double cosmicObjectSpeed;
+	private double cosmicObjectDistance;
+	private double cosmicObjectAge;
+	private int cosmicObjectSatellites;
+	private boolean isCosmicObjectObservable;
+
+	public CosmicObject (String id, double age) {
+		cosmicObjectId = id;
+		cosmicObjectAge = age;
+		
+	}
+    	  
+	public CosmicObject (double diam, double squar, double mass, double speed) {
+		cosmicObjectDiameter = diam;
+		cosmicObjectSquare = squar;
+		cosmicObjectMass = mass;
+		cosmicObjectSpeed = speed;	
+    }
+		// getter setter for ID
+	public String getCosmicObjectId() {
+    	return cosmicObjectId;
+    }
+    
+    public void setCosmicObjectId (String cosmicObjectId) {
+    	this.cosmicObjectId=cosmicObjectId;
+    }
+	    
+    // getter setter for prevailing element
+    public String getCosmicObjectPrevailimgElement() {
+    	return cosmicObjectPrevailimgElement;
+    }
+    
+    public void setCosmicObjectPrevailimgElement (String cosmicObjectPrevailimgElement) {
+    	this.cosmicObjectPrevailimgElement=cosmicObjectPrevailimgElement;
+    }
+	    
+    // getter setter for diameter
+    public double getCosmicObjectDiameter() {
+    	return cosmicObjectDiameter;
+    }
+    
+    public void setCosmicObjectDiameter (double cosmicObjectDiameter) {
+    	this.cosmicObjectDiameter=cosmicObjectDiameter;
+    }
+        
+    // getter setter for square
+    public double getCosmicObjectSquare() {
+    	return cosmicObjectSquare;
+    }
+    
+    public void setCosmicObjectSquare (double cosmicObjectSquare) {
+    	this.cosmicObjectSquare=cosmicObjectSquare;
+    }
+    
+        // getter setter for mass
+    public double getCosmicObjectMass() {
+    	return cosmicObjectMass;
+    }
+    
+    public void setCosmicObjectMass (double cosmicObjectMass) {
+    	this.cosmicObjectMass=cosmicObjectMass;
+    }
+    
+        // getter setter for speed
+    public double getCosmicObjectSpeed() {
+    	return cosmicObjectSpeed;
+    }
+    
+    public void setCosmicObjectSpeed (double cosmicObjectSpeed) {
+    	this.cosmicObjectSpeed=cosmicObjectSpeed;
+    }
+    
+    public static void main(String[] args) {
+        
+    CosmicObject mySample = new CosmicObject("mw12s2c7", 2.1);
+    CosmicObject mySample2 = new CosmicObject("an45s8c4", 0.45);
+    CosmicObject mySample3 = new CosmicObject(32.4, 1340, 46504, 42);
+    
+    mySample.setCosmicObjectPrevailimgElement("Iridium");
+    
+    
+    
+    System.out.println("\n"+"Cosmic object ID is: "+mySample.getCosmicObjectId()+". Pprevailing chemical element: "+mySample.getCosmicObjectPrevailimgElement()+
+    		". Age in millions of years: "+mySample.cosmicObjectAge);
+    
+    System.out.println("\n"+"Cosmic object ID is: "+mySample2.getCosmicObjectId()+". Pprevailing chemical element: "+mySample2.getCosmicObjectPrevailimgElement()+
+    		". Age in millions of years: "+mySample2.cosmicObjectAge);
+    
+    System.out.println("\n"+"Cosmic object diameter in km is: "+mySample3.getCosmicObjectDiameter()+". Cosmic object square in square km is: "+mySample3.getCosmicObjectSquare()+
+    		". Cosmic object total mass in millions of kg is: "+mySample3.getCosmicObjectMass()+". Cosmic object speed in km/hour is: "+
+    		mySample3.getCosmicObjectSpeed());
+    
 }
+
+}
+
+
+	
